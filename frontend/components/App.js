51456c9458450664461d349499502b152cbd3af9
Elizabeth Tutor <elitutor0116@gmail.com>
2023-03-20 19:58:07 -0700

    Completed MVP

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -17,14 +17,63 @@ class App extends React.Component {
           id: 1528817084358,
           completed: false,
         },
-        {
-          task: "Mow the lawn",
-          id: 1528817084358,
-          completed: false,
-        },
       ],
     };
   }
+
+  handleAdd = (task) => {
+    //1. SetState
+    //2. Changetodos
+    //3. MAke a copy todos
+    //4. Add a new todo to the end of oiur todo list.
+
+    const newTodo = {
+      task: task,
+      id: Date.now(),
+      coompleted: false,
+    };
+
+    this.setState({
+      ...this.state,
+      todos: [...this.state.todos, newTodo],
+    });
+  };
+
+  handleClear = () => {
+    //1. Set State
+    //2. loop through all todos
+    //3. remove all todos that have completed === true
+    //4. save filtered todos to state.
+
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.filter((todo) => {
+        return todo.completed === false;
+      }),
+    });
+  };
+
+  handleToggle = (clickedId) => {
+    //1. setState.
+    //2. Change todos.
+    //3. Find the todo that we clicked on.
+    //4. Flip the value of completed for that todo.
+    //5. We want to keep all other todos the same.
+
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === clickedId) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+
+        return todo;
+      }),
+    });
+  };
 
   render() {
     const { todos } = this.state;
@@ -32,11 +81,11 @@ class App extends React.Component {
       <div>
         <h1>Todos</h1>
 
-        <TodoList todos={todos} />
+        <TodoList handleToggle={this.handleToggle} todos={todos} />
 
-        <TodoForm todos={todos} />
+        <TodoForm handleAdd={this.handleAdd} />
 
-        <button>Clear</button>
+        <button onClick={this.handleClear}>Clear</button>
       </div>
     );
   }
